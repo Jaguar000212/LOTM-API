fba22e5b3733a16f4fa74f6c75e3e9cfe14a8823
Shryansh Parashar <Jaguar000212@gmail.com>
2024-02-04 07:10:54 +0530

    Update __init__.py
    Corrected typo

--- mystic/__init__.py
+++ mystic/__init__.py
@@ -1,6 +1,6 @@
 '''
 Author: Jaguar000212 & Theroid00
-Description: This is a library that scraps data from one of the fandoms of Lord of the Mystries, the renounced Novel.
+Description: This is a library that scraps data from one of the fandoms of Lord of the Mysteries, the renounced Novel.
 Data is scraped from https://lordofthemysteries.fandom.com/wiki/Lord_of_Mysteries_Wiki.
 '''
     
